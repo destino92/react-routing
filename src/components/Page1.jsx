319585b0de3dfff788ae58a998f908521e8cedec
destino92 <destinodello@gmail.com>
2016-08-14 19:11:40 +0300

    feat: define Page1 and Page2 component

---
+++ src/components/Page1.jsx
@@ -0,0 +1,12 @@
+var react = require('react');
+
+var Page1 = React.createClass({
+    render: function() {
+        return (
+            <h1>Page 1</h1>
+        );
+    }
+});
+
+
+module.exports = Page1;
